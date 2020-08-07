fba8fdc95214895d58926db953dc80787d8c06b3
Luciano43610 <luciano640@hotmail.com>
2020-08-06 22:34:20 -0300

    Cambios realizados en warcoin
    Cambio y ajustes, en variables y logica

--- Warcoin.cpp
+++ Warcoin.cpp
@@ -1,61 +1,33 @@
-/*El jugador ingresa la cantidad mínima y la cantidad máxima de monedas que se puede 
-sacar en cada turno. 
-Luego de realizado esto, la computadora elegirá un valor al azar entre 10 y 50, 
-que representará. 
+/*El jugador ingresa la cantidad mínima y la cantidad máxima de monedas que se puede
+sacar en cada turno.
+Luego de realizado esto, la computadora elegirá un valor al azar entre 10 y 50,
+que representará.
 La cantidad inicial de monedas que tendrá la pila.*/
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
 main(){
-	
-	int ingre1,ingre2,num,azar,retUsu,cant=0;
-	int cpu,cant2=0;       
-	
-	printf(" WARCOIN \t\n ======= ");
-	printf("\nIngrese la cantidad Minima de Monedas que se pueden Retirar:");
-	scanf("%d", &ingre1);
-	printf("Ingrese la cantidad Maxima de Monedas que se pueden Retirar:");	 
-	scanf("%d", &ingre2);
-	system("cls");	
-	system("pause");
-	
-	if(ingre2>ingre1){
-		printf(" WARCOIN \t\n ======= ");
-		printf("\nLa cantidad Minima de Monedas que se pueden Retirar:%d",ingre1);
-		printf("\nLa cantidad Maxima de Monedas que se pueden Retirar:%d",ingre2);
-		srand(time(NULL));
-	
-		num=(rand()%40)+11;
-		
-		printf("\nLa cantidad inicial de monedas en la pila es:%d",num);
-		srand(time(NULL));
-		azar=(rand()%2)+1;
-		printf("\nAleatoriamente se Eligira quien inicia el juego es ==>");
-		
-			if(azar==1){
-				printf("\tEl Usuario: ");
-				scanf("%d",&retUsu);
-			if(retUsu>=ingre1 && retUsu<=ingre2){
-				printf("La cantidad de monedas a eliminar:\t %d", retUsu);
-				cant=num-retUsu;
-				printf("\nLas Monedas que Quedan son:%d",cant);
-			}
-			else{
-			printf("ERROR: se deben desapilar entre %d y %d monedas.",ingre1,ingre2);
-			}
-			
-				
-		}
-			else{
-				printf("\tLa Computadora");
-				srand(time(NULL));
-				cpu=ingre1+rand()%(ingre2-ingre1);
-				printf("La computadora saco %d monedas de la pila",cpu);
-				cant2=num-cpu;
-				printf("\nLas Monedas que Quedan son:%d",cant2);
-			
-		}
-	
 
-	}
-}
+	int rangoMin,rangoMax,num,azar,retUsu,cantMonedas=0;
+	int retPc;
+
+	printf("\t\t ======= WARCOIN ======= ");
+	printf("\nIngrese la cantidad minima de monedas que se pueden retirar:");
+	scanf("%d", &rangoMin);
+	printf("Ingrese la cantidad maxima de monedas que se pueden retirar:");
+	scanf("%d", &rangoMax);
+    system("pause");
+	system("cls");
+
+
+	printf(" \nWARCOIN \t\n ======= ");
+    //printf("\nLa cantidad minima de monedas que se pueden retirar: %d",rangoMin);
+	//printf("\nLa cantidad maxima de monedas que se pueden retirar: %d",rangoMax);
+	//Genera numero entre el 10 y el 50
+	srand(time(NULL));
+	num=(rand()%40)+11;
+	//Genera un numero, que puede ser el 1 o el 2, que nos indicara quien comenzara
+	srand(time(NULL));
+	azar=(rand()%2)+1;
+	printf("\nInicia la partida ==> ");
+    cantMonedas=num;
