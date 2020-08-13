95ba7c85846d91b75ac9fddedcb4f8dca6f3176b
Luciano43610 <luciano640@hotmail.com>
2020-08-13 19:34:10 -0300

    Actualizacion del ahorcadito
    Prueba del codigo

--- Prueba del ahorcado algoritmo.cpp
+++ Prueba del ahorcado algoritmo.cpp
@@ -1,22 +1,17 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+
+void inicializar(char jue[]);
+void muestra(char v[]);
+void busqueda(char pa[], char jue[], char x);
 
 
 main(){
 	
-	char *palabras[] = {"Compilador", "Entorno", "Computadora", "Estructura", "Seleccion", "Repeticion", "Directiva", "Algoritmo", "Program", "Ejecucion", "Int", "Float", "Char", "Double", 
-	"Long", "If", "For", "While", "Return", "Break", "Switch", "Case", "Main", "Include", "Define", "Struct", "Cin"};
-	int numero=0, longitud=0;
-	srand(time(NULL));
-	numero=rand()%27; // generar un numero aleatorio entre 0 y 27
-	longitud=strlen(palabras[numero]); // obtengo la longitud de la palabra elegida aleatoriamente
-	//fflush(stdin);
-	char seleccionado [longitud];
-	printf("Logitud: %d\nPalabra: %s\n", longitud, palabras[numero]);
-	strcpy(seleccionado, palabras[numero]); //guardo letro por letra de la palabra elegida aleatoriamente
-	for(int i=0; i<longitud; i++){
-		printf("\n%c", seleccionado[i]);
-	}
-}
+	
+	char palabra[6];
+	char juego[6];
+	char x='a';
+	int c=0;
