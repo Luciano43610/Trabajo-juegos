a23b5c551b60d33b74ea661094636129d33170db
Luciano43610 <luciano640@hotmail.com>
2020-08-16 21:20:07 -0300

    Ahorcadito modificado
    parte inicial

--- Prueba del ahorcado algoritmo.cpp
+++ Prueba del ahorcado algoritmo.cpp
@@ -1,17 +1,38 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <conio.h>
-
-void inicializar(char jue[]);
-void muestra(char v[]);
-void busqueda(char pa[], char jue[], char x);
+#include <stdio.h>
 
 
+void MostrarPalabra();
+void IniciaJuego();
+void IngresoLt(char letra);
+void muestra(int v);
+char palabra[15];
+char palabraCompleta[15];
+int longitud=0;
 main(){
-	
-	
-	char palabra[6];
-	char juego[6];
-	char x='a';
-	int c=0;
+    char letraI, letra;
+    int i=0;
+    int c=0;
+    printf("~INGRESE 0 PARA TERMINAR~\n\n\nIngrese la palabra letra por letra: \n");
+
+    while(letraI!='0'){
+    scanf("%c", &letraI);
+        if(letraI<'A' || letraI>'Z' || letraI=='0'){
+            _flushall();
+            if(letraI=='0'){
+                printf("\nFin de la carga de la palabra.\nLongitud de la palabra: %d\n\n", longitud);
+            }
+            else{
+                if(letraI!='0'){
+                _flushall();
+                palabra[c]=letraI;
+                longitud++;
+                }
+                else{
+                    if(letraI<'A' || letraI>'Z'){
+                        printf("Ingrese un caracter valido.\n");
+                    }
+                }
+            }
+        }
+        c++;
